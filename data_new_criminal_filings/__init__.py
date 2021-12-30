62e4e91c11ab2f12565915e4143f4f4763b58b54
Nick Hand <nicholas.adam.hand@gmail.com>
2021-12-30 11:35:35 -0500

    switch to poetry

---
+++ data_new_criminal_filings/__init__.py
@@ -0,0 +1,6 @@
+from importlib.metadata import version
+from pathlib import Path
+
+__version__ = version(__package__)
+
+DATA_DIR = Path(__file__).parent.absolute() / ".." / "data"
---
+++ data_new_criminal_filings/__main__.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import click
+import pandas as pd
+from loguru import logger
+from phl_courts_scraper.new_filings import NewFilingsScraper
+
+from . import DATA_DIR
+
+
+@click.group()
+@click.version_option()
+def cli():
+    """Processing data for the New Criminal Filings."""
+    pass
+
+
+@cli.command()
+def update():
+    """Scrape the latest data and update the local data files."""
+    # Initialize the scraper and get the data
+    scraper = NewFilingsScraper()
+    data = scraper().to_pandas()
+
+    # Save latest dat
+    SORT_COLUMNS = ["filing_date", "docket_number", "defendant_name"]
+    data.sort_values(SORT_COLUMNS).to_csv(
+        DATA_DIR / "raw" / "latest-data.csv", index=False
+    )
+
+    # Save combined database
+    path = DATA_DIR / "processed" / "daily-data-historical.csv"
+    filename = Path(path)
+
+    # Merge together
+    if filename.exists():
+        data = pd.concat([data, pd.read_csv(filename)])
+
+    # Remove duplicates and save
+    original_length = len(data)
+    data = data.drop_duplicates()
+    logger.info(f"Removed {original_length - len(data)} duplicate filings")
+
+    # Save
+    data.sort_values(SORT_COLUMNS).to_csv(filename, index=False)
