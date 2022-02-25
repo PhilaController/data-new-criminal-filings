4138959b9472f3e8213319dc134b52b5c262d39e
Nick Hand <nicholas.adam.hand@gmail.com>
2022-02-25 15:14:24 -0500

    daily update

--- data_new_criminal_filings/core.py
+++ data_new_criminal_filings/core.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from phl_courts_scraper.new_filings import NewFilingsScraper
 
 from . import DATA_DIR
 
@@ -18,3 +19,38 @@ def load_historical_data():
         validate="m:1",
         how="left",
     )
+
+
+def scrape_new_filings():
+    """Scrape the latest filings."""
+
+    # Initialize the scraper and get the new data
+    scraper = NewFilingsScraper()
+    new_data = scraper().to_pandas()  # Save scraped columns
+
+    # Format date
+    new_data["filing_date"] = pd.to_datetime(new_data["filing_date"])
+
+    # Save latest data
+    SORT_COLUMNS = ["filing_date", "docket_number", "defendant_name"]
+    new_data.sort_values(SORT_COLUMNS).to_csv(
+        DATA_DIR / "raw" / "latest-data.csv", index=False
+    )
+
+    # Make combined database
+    filename = DATA_DIR / "processed" / "daily-data-historical.csv"
+
+    # Merge together
+    if filename.exists():
+        data = pd.concat([new_data, pd.read_csv(filename, parse_dates=["filing_date"])])
+    else:
+        data = new_data.copy()
+
+    # Remove duplicates
+    original_length = len(data)
+    data = data.drop_duplicates()
+    logger.info(f"Removed {original_length - len(data)} duplicate filings")
+
+    # Save
+    data = data.sort_values(SORT_COLUMNS)
+    data.to_csv(filename, index=False)
